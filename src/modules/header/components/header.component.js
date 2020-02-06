3b99a225ac7fa9a78b23a6e66a2c10656d28212e
Bladi <bladimaltego@gmail.com>
2020-02-06 21:26:17 +0100

    feat: added header.component

---
+++ src/modules/header/components/header.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header, Image } from 'semantic-ui-react'
+ 
+
+const HeaderExampleUsersIcon = () => (
+  <div>
+    <Header as='header' block>
+    </Header>
+    <Image
+      centered
+      size='large'
+      src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTs-itIzfjc12rBTEXpPRVNKWIIu8A46hPfR4Y_bgs-Y_7oBKwt'
+    />
+  </div>
+)
+
+export default HeaderExampleUsersIcon
